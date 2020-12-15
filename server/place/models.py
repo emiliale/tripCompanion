1c851b677920dbc4de3a08cc5d8b426a58ac1539
emiliale <54060962+emiliale@users.noreply.github.com>
2020-12-15 23:46:28 +0100

    Fix some mistakes

--- server/place/models.py
+++ server/place/models.py
@@ -6,3 +6,6 @@ class Place(models.Model):
     name = models.CharField(max_length=255)
     lng = models.FloatField(default=0)
     lat = models.FloatField(default=0)
+    lat = models.FloatField(default=0)
+    duration = models.IntegerField(default=0)
+    distance = models.FloatField(default=0)
